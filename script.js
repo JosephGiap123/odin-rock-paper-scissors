5774f082a5640aed1a613a11304a07cf9ed270ee
Joseph Giap <145407086+JosephGiap123@users.noreply.github.com>
2024-09-05 17:03:32 -0500

    Added getHumanChoice function and scores variable

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+console.log(humanScore);
+console.log(computerScore);
+console.log(getHumanChoice());
 console.log(getComputerChoice());
 
 
@@ -7,5 +11,14 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-
+    let humanChoice = prompt("Choose Rock, Paper or Scissors (Type in Rock, Paper or Scissors");
+    switch(humanChoice.toLowerCase()){
+        case "rock":
+            return 1;
+        case "paper":
+            return 2;
+        case "scissors":
+            return 3;
+        default: return 1;
+    }
 }
